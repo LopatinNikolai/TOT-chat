1144fd47dbd7369c807a3004267a59f89ee3ca62
LopatinNikolai <lopatin.nikolai@mail.ru>
2020-05-03 19:04:31 +0500

    onKeyPressEnter

--- src/components/MessageInput/MessageInput.tsx
+++ src/components/MessageInput/MessageInput.tsx
@@ -3,31 +3,46 @@ import "./MessageInput.css";
 import { Button } from "../Button/Button";
 
 type TMessageInputProps = {
-  onSend:(text:string)=>void
+  onSend: (text: string) => void;
 };
 
-export const MessageInput: React.FC<TMessageInputProps> = ({
-onSend
-}) => {
-
-  const [textAreaValue,setTextAreaValue]=React.useState("");
+export const MessageInput: React.FC<TMessageInputProps> = ({ onSend }) => {
+  const [textAreaValue, setTextAreaValue] = React.useState("");
 
   function handleClick() {
-    if (textAreaValue.trim()){
-        onSend(textAreaValue.trim());
+    const trimmed = textAreaValue.trim();
+    if (trimmed) {
+      onSend(trimmed);
     }
     setTextAreaValue("");
   }
 
- function handleChange(event:React.ChangeEvent<HTMLTextAreaElement>) {
+  function onEnter(event: React.KeyboardEvent<HTMLTextAreaElement>) {
+    if (event.key === "Enter" && !event.shiftKey) {
+      const trimmed = textAreaValue.trim();
+      if (trimmed) {
+        onSend(trimmed);
+      }
+      setTextAreaValue("");
+      event.preventDefault();
+    }
+  }
+
+  function handleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
     setTextAreaValue(event.target.value);
   }
 
   return (
     <div className="message-input">
-        <textarea value={textAreaValue} placeholder="напишите сообщение" name="message-input" className= "textarea" onChange={handleChange}></textarea>
-        <Button onClick={handleClick}>отправить</Button>
+      <textarea
+        value={textAreaValue}
+        placeholder="напишите сообщение"
+        name="message-input"
+        className="textarea"
+        onChange={handleChange}
+        onKeyPress={onEnter}
+      ></textarea>
+      <Button onClick={handleClick}>отправить</Button>
     </div>
-    
   );
 };
